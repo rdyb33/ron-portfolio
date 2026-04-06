8391434f69278644165b31e68a7f74db0648de2e
rdyb33 <rbulischeck@gmail.com>
2026-04-05 20:35:04 -0500

    fixed content layer issue

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro:loaders';
 
 const projects = defineCollection({
-	type: 'content',
+	loader: glob({ pattern: '**/[^_]*.{md,mdoc}', base: './src/content/projects' }),
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
